9be3ad37ff2c6542368d2367dfba6e39d66f230c
Mist <114344332+MIST145@users.noreply.github.com>
2026-02-11 23:02:31 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,35 +1,22 @@
---[[ ===================================================== ]]--
---[[      MH Vehicle Images V2 - FIXED & OPTIMIZED       ]]--
---[[              by MaDHouSe79 - v2.1.0                  ]]--
---[[ ===================================================== ]]--
-
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'MaDHouSe79'
-description 'MH Vehicle Images V2 - Sistema Dinâmico de Gestão de Imagens de Veículos (Fixed & Optimized)'
+author 'Mist_Goat'
+description 'MIST Vehicle Images V2 - Sistema Dinâmico de Gestão de Imagens de Veículos (Fixed & Optimized)'
 version '2.1.0'
-repository 'https://github.com/MaDHouSe79/mist-vehicleimages'
+repository 'https://github.com/MIST145/mist-vehicleimages'
 
-dependencies {
-    '/server:5848',
-    '/onesync',
-}
-
---[[ Shared Scripts ]]--
 shared_scripts {
     'config.lua'
 }
 
---[[ Client Scripts ]]--
 client_scripts {
     'shared/framework.lua',
     'client/main.lua',
     'client/exports.lua'
 }
 
---[[ Server Scripts ]]--
 server_scripts {
     'shared/framework.lua',
     'server/logger.lua',
@@ -37,7 +24,6 @@ server_scripts {
     'server/main.lua'
 }
 
---[[ UI (NUI) ]]--
 ui_page 'nui/index.html'
 
 files {
@@ -45,10 +31,4 @@ files {
     'nui/style.css',
     'nui/script.js',
     'data/vehicles.json'
-}
-
-escrow_ignore {
-    'config.lua',
-    'shared/*.lua',
-    'data/*.json'
 }
